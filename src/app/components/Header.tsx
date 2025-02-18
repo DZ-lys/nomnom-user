68b288de783384e37b3db905cf93c97b98c0df42
24LP2437 24LP2437 <24LP2437@G304-CC0216.local>
2025-02-18 09:27:19 +0800

    user push

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,17 @@
+const Headie = () => {
+  return (
+    <header className="bg-[#18181b] w-[100vw] h-[4.25rem] ">
+      <div className="flex w-[9.1rem] h-[2.8rem] gap-[10px] items-center ">
+        <div>
+          <img src="nomnom-Logo.png" alt="" />
+        </div>
+        <div>
+          <img src="nomnom-title.png" alt="" />
+        </div>
+      </div>
+      <div></div>
+    </header>
+  );
+};
+
+export default Headie;
