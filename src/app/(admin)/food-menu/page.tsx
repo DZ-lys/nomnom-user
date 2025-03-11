347637e210637f69f4145deab192d60d1e238f21
24LP2437 24LP2437 <24LP2437@G304-CC0216.local>
2025-03-11 11:25:23 +0800

    test

--- src/app/(admin)/food-menu/page.tsx
+++ src/app/(admin)/food-menu/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+import { useEffect } from "react";
+
 const Food_Menu = () => {
-  return <div className="flex">sup</div>;
+  useEffect(() => {
+    const getCategories = async () => {
+      const data = await fetch("http://localhost:7800/addCategory");
+      const jsonData = await data.json();
+      console.log(jsonData);
+    };
+    getCategories();
+  });
+
+  return (
+    <div className="flex flex-col px-9 gap-6 bg-slate-100 w-[100vw] ">
+      <div className=" mt-16 max-w-[73.188rem] h-[9.625rem] bg-white top-6 left-56 gap-6 "></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default Food_Menu;
--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -7,9 +7,28 @@ type Props = {
 
 const SideBar = () => {
   return (
-    <div className="flex flex-col gap-4 bg-slate-700 w-[20%] min-h-[100vh] ">
+    <div className="flex flex-col gap-8 w-[20%] min-h-[100vh] py-9 px-7 ">
       <div>
-        <LogoHorizontal />
+        <div className="w-36 h-11 flex items-center gap-2 ">
+          <div className="overflow-hidden w-11 h-9 ">
+            <img src="nomnom-Logo.png" alt="" />
+          </div>
+          <div>
+            <div className="flex">
+              <h4 className="text-xl font-semibold text-[#000000] tracking-[-0.5px] ">
+                Nom
+              </h4>
+              <h4 className="text-xl font-semibold text-[#000000] tracking-[-0.5px] ">
+                Nom
+              </h4>
+            </div>
+            <div>
+              <p className="text-[#303031c5] text-xs font-normal ">
+                swift delivery
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="flex flex-col gap-2">
         <h2>Food Menu</h2>
